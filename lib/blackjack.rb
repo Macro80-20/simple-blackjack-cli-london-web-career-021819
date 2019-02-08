823025055921a33c430b562af0eb77e65022e8a3
fernando <fernando_lopezc@live.com>
2019-02-08 10:55:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,9 +60,7 @@ welcome
 first_cards = initial_round #we register there first round of cards
 while first_cards <21 #while it is less than 21
     first_cards = hit?(first_cards)  # there hand will convert to a new number if they clicked hit witin the method and return it up until they said miss
-    first_cards = display_card_total(first_cards)  #we display the number they got . 
-end 
+    first_cards = display_card_total(first_cards)  #we display the number they got .
+end
 end_game(first_cards)
 end
-
-
