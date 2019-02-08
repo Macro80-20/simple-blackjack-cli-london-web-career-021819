073a285b5d673445056c32f6d5a32a9dd924a349
fernando <fernando_lopezc@live.com>
2019-02-08 11:02:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,11 +57,9 @@ def runner
   welcome
 initial_round
 card_total = initial_round
-
-  while card_total < 21
+while card_total < 21
     card_total = hit?(card_total)
     card_display = display_card_total(card_total)
   end
   end_game(card_total)
-
 end
